fe2ecdbdb32f71f51763b281bb2ebb28c090e9af
markp-netengineer <markphillipkirabo@gmail.com>
2024-02-16 00:35:05 -0800

    1

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- scri[t that lists all privilleges of users on my server 
+SHOW GRANT FOR 'user_0d_1' @ 'localhost';
+SHOW GRANT FOR 'user_0d_2' @ 'localhost';
---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,9 @@
+--Script the ceate a user 
+CREATE USER 
+ IF NOT EXISTS 'user_0d_1'@'localhost'
+ IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+ ON *.*
+ TO 'user_0d_1'@'localhost'
+ IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
